4c4d9964b3351429e96f539d614491dce00eebf4
Deniz Ök <deniz.ok2019@gmail.com>
2023-07-17 12:48:42 +0300

    added billing and cardDeal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 
 const Button = () => {
   return (
-    <button type="button" className={`py-4 px-6 font-poppins font-medium bg-blue-gradient text-[18px] text-primary outline-none mt-10`}>
+    <button type="button" className={`py-4 px-6 font-poppins font-medium bg-blue-gradient text-[18px] text-primary outline-none mt-10 rounded-[10px]`}>
       Get Started
     </button>
   )
